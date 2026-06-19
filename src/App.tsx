df6aa28f9999243e128ea9eac8f3fd905c65cae2
nezabootkin <nezabootk1n@gmail.com>
2026-06-19 19:29:38 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -166,7 +166,7 @@ function App() {
                 backgroundPosition: 'center'
               }}
             >
-              Michurin Club - 16:00
+              Michurin Club - 17:00
             </text>
             <div className={isMobile ? 'places_mobile' : 'places'}>
               <img
